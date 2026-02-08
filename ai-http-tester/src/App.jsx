619e853ffb85d07aa902b52b015d309fadb3b0b6
Lance <dolance12345@gmail.com>
2026-02-07 18:32:47 -0600

    Split the pages and header into folders

--- ai-http-tester/src/App.jsx
+++ ai-http-tester/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Sidebar } from "./pages/Sidebar";
+import { Editor } from "./pages/Editor";
+import { History } from "./pages/History";
+import "./App.css";
 
 function App() {
-  
+  const [rawRequest, setRawRequest] = useState("");
 
   return (
     <>
+      <p>hello world</p>
+      <Header />
+      <Sidebar />
+      <Editor />
+      <History />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
